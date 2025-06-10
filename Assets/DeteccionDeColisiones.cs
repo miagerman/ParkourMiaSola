88140fbb4b96a485faea9c4f52f7f84d7a563c27
miagerman <49194476@est.ort.edu.ar>
2025-06-10 10:25:49 -0300

    .

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+<<<<<<< HEAD
+=======
+    // Start is called before the first frame update
+>>>>>>> 58a756213431fa7f86a843f07946460a7ff738fd
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 58a756213431fa7f86a843f07946460a7ff738fd
+}
---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+<<<<<<< HEAD
+    public float cost;
+    public MoneyManager moneyManager;
+
+    void Start ()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            // actualizar dinero por la compra
+            moneyManager.UpdateMoney(-cost);
+            Destroy(gameObject);
+        }
+        
+=======
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+>>>>>>> 58a756213431fa7f86a843f07946460a7ff738fd
+    }
+}
